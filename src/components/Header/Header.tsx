c5003f9a833531d637c0b3cc7cac533b7f4c223d
AlexDvor <alexmatvichuk@gmail.com>
2026-01-27 12:49:16 +0100

    set project

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import type { FC } from 'react';
+
+type IHeader = object;
+
+const IHeader: FC<IHeader> = () => {
+	return <></>;
+};
+
+export default IHeader;
---
+++ src/pages/Home/HomePage.tsx
@@ -0,0 +1,9 @@
+import type { FC } from 'react';
+
+type IHomePage = object;
+
+const HomePage: FC<IHomePage> = () => {
+	return <></>;
+};
+
+export default HomePage;
