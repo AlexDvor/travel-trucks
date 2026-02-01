04ff35fc649ec31432fe007ca489015ff74b1695
AlexDvor <alexmatvichuk@gmail.com>
2026-02-01 21:32:10 +0100

    add persist

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,19 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 import { Provider } from 'react-redux';
 
-import { store } from './store/store.ts';
+import { persistor, store } from './store/store.ts';
 import './index.css';
 import App from './App.tsx';
+import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<Provider store={store}>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
+			<PersistGate persistor={persistor}>
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</PersistGate>
 		</Provider>
 	</StrictMode>,
 );
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,14 +2,43 @@ import { configureStore } from '@reduxjs/toolkit';
 import filterSliceReducer from './filterSlice';
 import camperSliceReducer from './campersSlice';
 import favoriteSliceReducer from './favoriteSlice';
+import {
+	persistStore,
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
+const favoritePersistConfig = {
+	key: 'favorite',
+	storage,
+	whitelist: ['campers'],
+};
+
+const persistedFavoriteReducer = persistReducer(
+	favoritePersistConfig,
+	favoriteSliceReducer,
+);
 export const store = configureStore({
 	reducer: {
 		campers: camperSliceReducer,
 		filters: filterSliceReducer,
-		favorite: favoriteSliceReducer,
+		favorite: persistedFavoriteReducer,
 	},
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
